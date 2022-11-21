21075e1951ce0521a8f0f7672c10aea96f1a3407
Swhag <khg5293@outlook.com>
2022-11-21 00:07:53 -0500

    update

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: {
-    app: './src/index.js',
+    app: './src/modules/index.js',
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
